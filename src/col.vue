f3a4ad98191fbe03fd343ed4110bdbcd529fc7b2
renFly <ren_hf@qq.com>
2019-02-25 22:16:10 +0800

    完成Grid组件功能

---
+++ src/col.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="col" :class="colClass" :style="gutterStyle">
+    <div class="showBackgroundColor">
+      <slot></slot>
+    </div>
+  </div>  
+</template>
+<script>
+export default {
+  data(){
+    return {
+      gutter: null
+    }
+  },
+  props: ['span','order','offset'],
+  computed: {
+    colClass() {
+      let {span, offset, order} = this;
+      return [span && `col-${span}`, offset && `offset-${offset}`, order]
+    },
+    gutterStyle() {
+      let {gutter} = this
+      return {
+        paddingLeft: gutter/2 + 'px',
+        paddingRigt: gutter/2 + 'px'
+      }
+    }
+  }
+ 
+}
+</script>
+<style lang="scss" scoped>
+  .col {
+    width: 50%;
+    height: 60px;
+  
+    @for $n from 1 through 24 {
+      &.col-#{$n} {
+        width: ($n/24)*100%;
+      }
+    }
+
+    @for $n from 1 through 24 {
+      &.offset-#{$n} {
+        margin-left: ($n/24)*100%;
+      }
+    }
+    .showBackgroundColor {
+      background-color: pink;
+      text-align: center;
+      height: 60px;
+      line-height: 60px;
+    }
+  }
+   
+</style>
---
+++ src/row.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row" :style="gutterStyle">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props: ['gutter'],
+  computed: {
+    gutterStyle(){
+      let {gutter} = this
+      return {
+        marginLeft: -gutter/2 + 'px', 
+        marginRight: -gutter/2 + 'px'
+      }
+    }
+  },
+  mounted(){
+    this.$children.forEach((vm) => {
+      vm.gutter = this.gutter
+    })
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .row {
+    display: flex;
+  }
+</style>
