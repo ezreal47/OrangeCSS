ec539bb10229ede93c492640bdf7adfd7e054df3
renFly <ren_hf@qq.com>
2019-02-28 21:52:06 +0800

    完成部分mesaage组件功能

---
+++ src/message.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="message">
+    <svg class="icon">
+      <use :xlink:href="`#icon-${typeName}`"></use>
+    </svg>
+    <slot></slot>
+  </div>  
+</template>
+<script>
+
+export default {
+  data(){
+    return {
+      iconNames: ['info','success']
+    }
+  },
+  props: {
+    autoCloseDelay: {
+      type: Number,
+      default: 3
+    },
+    typeName: {
+      type: String
+    } 
+  },
+  mounted() {
+    setTimeout(() => {
+      this.close()
+    }, this.autoCloseDelay * 1000)  
+  },
+  methods: {  
+    close() {
+      this.$el.remove()
+      this.$destroy()
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .message {
+    border: 1px solid#ebedf0;
+    border-radius: 8px;
+    position: fixed;
+    top: 5%;
+    left: 50%;
+    transform: translateX(-50%);
+    padding: 8px 18px;
+    box-shadow: 1px 3px 20px -6px rgba(0,0,0,0.61);
+    text-align: center;
+    display: flex;
+    align-items: center;
+    
+    // justify-content: flex-end;
+    .icon {
+      width: 18px;
+      height: 18px;
+      fill: #47b8e0;
+      margin-right: 10px;
+    }
+  }
+</style>
---
+++ src/plugin.js
@@ -0,0 +1,36 @@
+import Message from './message'
+
+export default {
+  install(Vue, options){
+    Vue.prototype.$message = {
+      trigger(type, message){
+        let Constructor = Vue.extend(Message)
+        let vm = new Constructor({
+          propsData: {
+            typeName: type
+          }
+        })
+        
+        vm.$slots.default = [message]
+        vm.$mount()
+        document.body.appendChild(vm.$el)
+        console.log(vm.$props)
+      },
+
+      info(message){
+        this.trigger('info',message)
+      },
+      success(message){
+        this.trigger('success',message)
+      },
+      error(message){
+        this.trigger('error',message)
+      },
+      warning(message){
+        this.trigger('warning',message)
+      }
+      
+
+    }
+  }
+}
