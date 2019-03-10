ae87cd4a56abee6603802acf84f54da46d1a90d0
renFly <ren_hf@qq.com>
2019-03-10 17:14:50 +0800

    基本功能的collapse组件

---
+++ src/collapse/collapse-pane.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="collapse-pane">
+    <div class="title" @click="open = !open">
+      <slot></slot>
+    </div>
+    <div class="content" v-if="open">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      open: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.collapse-pane {
+  .title {
+    height: 38px;
+    line-height: 38px;
+    padding-left: 16px;
+    color: #666;
+    cursor: pointer;
+    position: relative;
+    border: 1px solid #dcdee2;
+    transition: all .2s ease-in-out;
+    margin-top: -1px;
+    margin-left: -1px;
+    margin-right: -1px;
+    display: flex;
+    align-items: center;
+  }
+  .content {
+    color: #515a6e;
+    padding: 16px;
+    background-color: #fff;
+  }
+}
+</style>
---
+++ src/collapse/collapse.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="collapse">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+  .collapse {
+    background-color: #f7f7f7;
+    border-radius: 4px;
+    border: 1px solid #dcdee2;
+    max-width: 900px;
+    overflow: hidden;
+  }
+</style>
