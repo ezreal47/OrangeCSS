ab64f7123534a68c1021aa82b1e477861313e35b
renFly <ren_hf@qq.com>
2019-03-17 14:46:13 +0800

    完成carousel组件

--- src/carousel-item.vue
+++ src/carousel-item.vue
@@ -28,9 +28,7 @@ export default {
 
 </script>
 <style lang="scss" scoped>
-.carousel-item {
-  // display: inline-block; 
-}
+.carousel-item {}
 .carousel-leave-active {
   position: absolute;
   left: 0;
--- src/carousel.vue
+++ src/carousel.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class="carousel">
+  <div class="carousel" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
     <div class="carousel-window" ref="window">
       <div class="carousel-wrapper">
          <slot></slot>
       </div>  
     </div>
     <div class="carousel-dots">
+      <span class="icon" @click="select(selectedIndex - 1)">
+        <svg><use xlink:href="#icon-left"></use></svg>
+      </span>
       <span v-for="n in childrenLength" :key="n.id" :class="{active: selectedIndex === n-1}"
       @click="select(n-1)">
         {{n}}
       </span>
+       <span class="icon" @click="select(selectedIndex + 1)">
+         <svg><use xlink:href="#icon-left-copy"></use></svg>
+       </span>
     </div>
   </div>  
 </template>
@@ -19,15 +25,16 @@ export default {
     selected: {
       type: String
     },
-    autoPlay: {
+    autoplay: {
       type: Boolean,
-      default: true
+      default: false
     }
   },
   data() {
     return {
       childrenLength: 0,
-      lastIndex: undefined
+      lastIndex: undefined,
+      timerId: undefined
     }
   },
   computed: {
@@ -40,7 +47,9 @@ export default {
   },
   mounted() {
     this.updateChildren()
-    // this.play()
+    if(this.autoplay) {
+      this.play()
+    }
     this.childrenLength = this.$children.length
   },
   updated() {
@@ -48,16 +57,26 @@ export default {
   },
   methods: {
     play() {
-      // const names = this.$children.map(vm => vm.name)
+      if(this.timerId) { return }
       let run = () => {
         let index = this.names.indexOf(this.getSelected())
-        let newIndex = index - 1
-        if(newIndex === -1) { newIndex = this.names.length - 1}
-        if(index === this.names.length) { newIndex = 0 }
+        let newIndex = index + 1
         this.select(newIndex)
-        setTimeout(run, 3000)
+        this.timerId = setTimeout(run, 3000)
       }
-      setTimeout(run, 3000)
+      this.timerId = setTimeout(run, 3000)
+    },
+    pause() {
+      window.clearTimeout(this.timerId)
+      this.timerId = undefined
+    },
+    onMouseEnter() {
+      this.pause()
+    },
+    onMouseLeave() {
+      if(this.autoplay) {
+        this.play()
+      }
     },
     getSelected() {
       let first = this.$children[0]
@@ -66,36 +85,63 @@ export default {
     updateChildren() {
       let result = this.getSelected()
       this.$children.forEach(vm => {
-        // vm.selected = result
-        vm.reverse = this.selectedIndex > this.lastIndex ? false : true
+        let reverse = this.selectedIndex > this.lastIndex ? false : true
+        if (this.timerId) {
+          if (this.lastIndex === this.$children.length - 1 && this.selectedIndex === 0) {
+            reverse = false
+          } if (this.lastIndex === 0 && this.selectedIndex === this.$children.length - 1) {
+            reverse = true
+          }
+        }
+        vm.reverse = reverse
         this.$nextTick(() => {
           vm.selected = result
         })
-        // const names = this.$children.map()
       })
 
     },
-    select(index) {
+    select(newIndex) {
       this.lastIndex = this.selectedIndex
-      this.$emit('update:selected', this.names[index])
+      if(newIndex === -1) { newIndex = this.names.length - 1}
+      if(newIndex === this.names.length) { newIndex = 0 }
+      this.$emit('update:selected', this.names[newIndex])
     }
   }
 }
 </script>
 <style lang="scss" scoped>
 .carousel {
-  border: 1px solid green;
-  width: 300px;
+  display: inline-block;
   &-window {
     overflow: hidden;
-    border: 1px solid blue;
-
+    position: relative;
   }
   &-wrapper {
     position: relative;
   }
-  &-dots > span {
-    &.active {color: red}
+  &-dots {
+    display: flex;
+    justify-content: center;
+    margin-top: 6px;
+    .icon {
+      transition: fill 0.2s;
+      background: #fff; fill: #46a1de;
+      &:hover {fill: #FFBC42}  
+    }
+    > span {
+      display: inline-flex;
+      width: 22px;
+      height: 22px;
+      border-radius: 50%;
+      justify-content: center;
+      align-items: center;
+      margin: 0 4px;
+      background-color: #46a1de;
+      font-size: 12px;
+      color: #fff;
+      cursor: pointer;
+      &.active {background-color: #FFBC42}
+    }
   }
 }
 </style>
